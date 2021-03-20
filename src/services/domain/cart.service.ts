426ae8372e07e92bea9380ba62329d8ae554d612
Pedro Paulo <silva.pedropaulosantos@gmail.com>
2021-03-19 23:25:10 -0300

    Acesso ao carrinho: botão flutuante e menu

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -25,11 +25,50 @@ export class CartService {
         addProduct(produto: ProdutoDTO): Cart {
             let cart = this.getCart();
             let position = cart.items.findIndex(x => x.produto.id == produto.id);
-            if(position ==-1) {
+            if(position == -1) {
                 cart.items.push({quantidade: 1, produto:produto});
             }
             this.storage.setCart(cart);
             return cart;
         }
+        removeProduct(produto: ProdutoDTO): Cart{
+            let cart = this.getCart();
+            let position = cart.items.findIndex(x => x.produto.id == produto.id);
+            if(position != -1) {
+                cart.items.splice(position, 1);
+            }
+            this.storage.setCart(cart);
+            return cart;
+        }
 
+        increaseQuantity(produto: ProdutoDTO): Cart {
+            let cart = this.getCart();
+            let position = cart.items.findIndex(x => x.produto.id == produto.id);
+            if(position != -1) {
+               cart.items[position].quantidade++;
+            }
+            this.storage.setCart(cart);
+            return cart;
+        }
+        decreaseQuantity(produto: ProdutoDTO): Cart {
+            let cart = this.getCart();
+            let position = cart.items.findIndex(x => x.produto.id == produto.id);
+            if(position != -1) {
+               cart.items[position].quantidade--;
+               if(cart.items[position].quantidade<1) {
+                   cart = this.removeProduct(produto);
+               }
+            }
+            this.storage.setCart(cart);
+            return cart;
+        }
+
+        getTotal(): number {
+            let cart = this.getCart();
+            let sum = 0;
+            cart.items.forEach( item => {
+                sum += item.produto.preco * item.quantidade;
+            });
+            return sum;
+        }
 }
