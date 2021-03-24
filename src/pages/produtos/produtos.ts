204a3d03b9d88db006c2b9d1839f0eddb1b53887
Pedro Paulo <silva.pedropaulosantos@gmail.com>
2021-03-24 01:53:12 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+   this.loadData();
+  }
+  
+  loadData(){
     let categoria_id =this.navParams.get('categoria_id');
     let loader = this.presentLoading();    
     this.produtoService.findByCategoria(categoria_id)
@@ -34,7 +38,7 @@ export class ProdutosPage {
       loader.dismiss();
     })
   }
-      
+
   loadImageUrls(){
     for(var i=0; i<this.itens.length; i++){
       let item = this.itens[i];
@@ -58,4 +62,12 @@ export class ProdutosPage {
     loader.present();
     return loader;
   }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }
+
 }
