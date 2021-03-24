83ff7db434830717d34f14193a2157957f573791
Pedro Paulo <silva.pedropaulosantos@gmail.com>
2021-03-24 00:52:55 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -21,6 +21,8 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -55,20 +57,28 @@ export class OrderConfirmationPage {
   }
 
   back() {
-    this.navCtrl.setRoot('CartPage')
+    this.navCtrl.setRoot('CartPage');
   }
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
 
+  }
   checkout(){
     this.pedidoService.insert(this.pedido)
       .subscribe(response =>{        
           this.cartService.createOrClearCart();
-          console.log(response.headers.get('location'));
+          this.codPedido = this.extractId(response.headers.get('location'));
       }, 
       error => {
         if(error.status == 403) {
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extractId(location: string) : string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position +1, location.length);
   }
 
 }
